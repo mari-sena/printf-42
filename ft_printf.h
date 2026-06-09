551235d4826f4bf731179f171280ab92c2847daf
Mariana Sena <mariana7007@hotmail.com>
2026-06-09 10:54:02 -0300

    putlong

--- ft_printf.h
+++ ft_printf.h
@@ -9,5 +9,6 @@ int	ft_putchar(char c);
 int	ft_putstr(char *s);
 int	ft_putptr(void *ptr);
 int	ft_putint(int nbr);
+int	ft_putlong(unsigned long nbr, char *base);
 
 #endif
---
+++ utils/ft_putlong.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putlong.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mariafer <mariafer@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/09 10:41:55 by mariafer          #+#    #+#             */
+/*   Updated: 2026/06/09 10:41:56 by mariafer         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../ft_printf.h"
+
+int	ft_putlong(unsigned long nbr, char *base)
+{
+	int		counter;
+	int		base_len;
+	int		i;
+	char	c[65];
+
+	counter = 0;
+	base_len = 0;
+	while (base[base_len])
+		base_len++;
+	if (nbr == 0)
+		return (ft_putchar('0'));
+	while (nbr > 0)
+	{
+		c[i++] = base[nbr % base_len];
+		nbr /= base_len;
+	}
+	while (i > 0)
+		counter += write(1, &c[--i], 1);
+	return (counter);
+}
