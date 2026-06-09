5e1896780f1b6b6318e1945dc157d552558f2bbf
Mariana Sena <mariana7007@hotmail.com>
2026-06-09 11:45:15 -0300

    fix case of spaces

--- ft_printf.c
+++ ft_printf.c
@@ -12,22 +12,22 @@
 
 #include "ft_printf.h"
 
-int	convert_cases(char c, va_list ap)
+int	convert_cases(char c, va_list *ap)
 {
 	if (c == 'c')
-		return (ft_putchar(va_arg(ap, int)));
+		return (ft_putchar(va_arg(*ap, int)));
 	else if (c == 's')
-		return (ft_putstr(va_arg(ap, char *)));
+		return (ft_putstr(va_arg(*ap, char *)));
 	else if (c == 'p')
-		return (ft_putptr(va_arg(ap, void *)));
+		return (ft_putptr(va_arg(*ap, void *)));
 	else if (c == 'd' || (c == 'i'))
-		return (ft_putint(va_arg(ap, int)));
+		return (ft_putint(va_arg(*ap, int)));
 	else if (c == 'u')
-		return (ft_putlong(va_arg(ap, unsigned int), "0123456789"));
+		return (ft_putlong(va_arg(*ap, unsigned int), "0123456789"));
 	else if (c == 'x')
-		return (ft_putlong(va_arg(ap, unsigned int), "0123456789abcdef"));
+		return (ft_putlong(va_arg(*ap, unsigned int), "0123456789abcdef"));
 	else if (c == 'X')
-		return (ft_putlong(va_arg(ap, unsigned int), "0123456789ABCDEF"));
+		return (ft_putlong(va_arg(*ap, unsigned int), "0123456789ABCDEF"));
 	else if (c == '%')
 		return (ft_putchar('%'));
 	return (0);
@@ -54,13 +54,14 @@ int	ft_printf(const char *s, ...)
 	va_start(ap, s);
 	while (*s)
 	{
-		if (*s == '%' && validate_cases("cspdiuxX", *(s + 1)))
+		if (*s == '%' && validate_cases("cspdiuxX%", *(s + 1)))
 		{
 			s++;
-			len += convert_cases(*s, ap);
+			len += convert_cases(*s, &ap);
 		}
 		else
 			len += ft_putchar(*s);
+		s++;
 	}
 	va_end(ap);
 	return (len);
